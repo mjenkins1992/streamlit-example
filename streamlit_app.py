acd7112b0bced1c1962db559d16426f9c6ada079
mjenkins1992 <mark.jenkins@gcu.ac.uk>
2022-06-10 12:08:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import time
 
 #"""
 ## Welcome to Streamlit!
@@ -48,41 +49,50 @@ def _max_width_():
 
 _max_width_()
 
-c30, c31, c32 = st.columns([2, 1.5, 1])
-
-#col for parameters
-
-#col for Upload
-with c31:
-    st.title("👆 Input Upload")
+c30, c31 = st.columns([.5, 1])
+#col for Input by File
+with c30:
+    st.header("👆 Upload File to Summarise")
     label = ""
     st.file_uploader(label)
+# Col for Input by text
+with c31:
+    st.header("⌨️ Enter Text to Summarise")
+    label = ""
+    st.button(label)
 
-#col for Download
-with c32:
-    st.title("👇 Output Download")
+c30, c31 = st.columns([.25, 1])
+#col for Start Button
+with c30:
+    label = "Generate Summary"
+    st.button(label)
+# Col for Prog Bars
+with c31:
+    st.progress(stat_val)
+
+st.title("👇 Output Download")
     st.header("")
     label = "Summary Download"
     data = "This is a temporary string"
     st.download_button(label, data, file_name="summary.txt")
 
 #with st.echo(code_location='below'):
-total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-Point = namedtuple('Point', 'x y')
-data = []
-
-points_per_turn = total_points / num_turns
-
-for curr_point_num in range(total_points):
-    curr_turn, i = divmod(curr_point_num, points_per_turn)
-    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-    radius = curr_point_num / total_points
-    x = radius * math.cos(angle)
-    y = radius * math.sin(angle)
-    data.append(Point(x, y))
-
-st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-    .mark_circle(color='#0068c9', opacity=0.5)
-    .encode(x='x:Q', y='y:Q'))
+#total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+#
+#Point = namedtuple('Point', 'x y')
+#data = []
+#
+#points_per_turn = total_points / num_turns
+#
+#for curr_point_num in range(total_points):
+#    curr_turn, i = divmod(curr_point_num, points_per_turn)
+#    angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#    radius = curr_point_num / total_points
+#    x = radius * math.cos(angle)
+#    y = radius * math.sin(angle)
+#    data.append(Point(x, y))
+#
+#st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#    .mark_circle(color='#0068c9', opacity=0.5)
+#    .encode(x='x:Q', y='y:Q'))
