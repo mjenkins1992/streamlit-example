6774b2f819ca25f4288c5b833be24adaab28cab6
mjenkins1992 <mark.jenkins@gcu.ac.uk>
2022-06-10 10:55:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ with c31:
 #col for Download
 with c32:
     st.title("👇 Output Download")
-    label = ""
+    label = "Summary Download"
     data = "This is a temporary string"
     st.download_button(label, data, file_name="summary.txt")
 
