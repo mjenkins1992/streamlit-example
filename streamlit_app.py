95320e454293d744c338e47ba8aa33a01f2fdd42
mjenkins1992 <mark.jenkins@gcu.ac.uk>
2022-06-23 14:39:14 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,4 +120,4 @@ with c31:
 # Results Section
 with c32:
     label = "✅ Lay Summary"
-    st.text_area(label, value=st.session_state.final_output)
+    st.text_area(label, value=st.session_state.final_output, disabled=True, placeholder='Your summary will appear here when it has been generated...')
