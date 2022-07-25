2dbeca242d6717ba18440f1082a8736bfc13f755
mjenkins1992 <mark.jenkins@gcu.ac.uk>
2022-07-25 12:18:26 +0100

    Test chunked input

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,25 +39,6 @@ def read_pdf(file):
         all_page_text += page.extractText()
     return all_page_text
 
-def run_analysis():
-    with st.spinner('Generating Summary...'):
-        if st.session_state["text_box"]:
-            raw_text = st.session_state.input_text
-        elif st.session_state["text_upload"]:
-            if st.session_state.input_file.type == "text/plain":
-                raw_text = str(st.session_state.input_file.read(),"utf-8")
-            elif st.session_state.input_file.type == "application/pdf":
-                raw_text = read_pdf(st.session_state.input_file)
-            elif st.session_state.input_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document" :
-                raw_text = docx2txt.process(st.session_state.input_file)
-
-        st.session_state.output_text = translator.translate(raw_text, src='en', dest='fr')
-        st.session_state.final_output = st.session_state.output_text.text
-        st.session_state.download_off = False
-        st.session_state.box_value = raw_text
-        #st.success('Complete!')
-    return
-
 def get_raw_txt():
     with st.spinner('Extracting Text...'):
         if st.session_state["text_box"]:
@@ -71,17 +52,17 @@ def get_raw_txt():
                 raw_text = docx2txt.process(st.session_state.input_file)
         st.session_state.box_value = raw_text
 
-def prep_model():
-    model.cuda(dev_id)
-
-#def gen_sample_input():
-
-
+max_input = 4096
 def generate_summary():
-    with st.spinner('Running Tokenizer...'):
+    with st.spinner('Preprocessing Input...'):
         raw_text = st.session_state.box_value
+        if len(raw_text) > max_input:
+            chunked_text = lambda raw_text, max_input: [raw_text[i:i+max_input] for i in range(0, len(raw_text), max_input)]
+        else:
+            chunked_text = raw_text
 
-        to_pred = tokenizer(raw_text, padding="max_length", max_length=4096, return_tensors="pt", truncation=True)
+    with st.spinner('Running Tokenizer...'):
+        to_pred = tokenizer(chunked_text, padding="max_length", max_length=4096, return_tensors="pt", truncation=True)
 
     with st.spinner('Pass tokens to GPU...'):
         input_ids=to_pred["input_ids"].cuda(dev_id)
@@ -97,14 +78,13 @@ def generate_summary():
     with st.spinner('Decode Results...'):
         st.session_state.final_output = tokenizer.batch_decode(predicted_ids, skip_special_tokens=True)
 
-def run_analysis2():
+def run_analysis():
     get_raw_txt()
     #Any preprocessing on raw text should happen here
-    prep_model()
+    model.cuda(dev_id)
     generate_summary()
     st.session_state.download_off = False
     st.success('Complete!')
-
     return
 
 def update_button():
@@ -130,8 +110,12 @@ st.title("📄 Lay Summary Generator")
 with st.expander("ℹ️ - About this app", expanded=True):
     st.write(
         """
-        - Some blurb here about the app
-        - Usage instructions
+        - This application uses the Longformer Transformer NLP Algorithm to generate Lay Summaries of Clinical Trial Reports.
+        - Select Your Data Input Method
+        - File Upload accepts PDF, DOCX and TXT
+        - Click Generate Summary to run the model
+        - The Download will become available when the summary is complete
+        - To run a second summary or fix any GUI issues refresh the page
         """
     )
 
@@ -197,13 +181,12 @@ c30, c31, c32 = st.columns([.25, .25, 1])
 # Col for Start Button
 with c30:
     label = "Generate Summary"
-    st.session_state.run = st.button(label, on_click=run_analysis2, disabled=st.session_state.generate_button)
+    st.session_state.run = st.button(label, on_click=run_analysis, disabled=st.session_state.generate_button)
 
 with c31:
     label = "Download Summary"
     st.download_button(label, ''.join(st.session_state.final_output), file_name="summary.txt", disabled=st.session_state.download_off)
 
 # Results Section
-
 label = "✅ Lay Summary"
 st.text_area(label, value=st.session_state.final_output, disabled=True, height=160, placeholder='Your summary will appear here when it has been generated...')
